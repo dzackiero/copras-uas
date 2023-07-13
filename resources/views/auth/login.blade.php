2b2dbbe5b3882e3f99dd39036dfd49c27e7a6c10
dzackiero <dzakynashshar@gmail.com>
2023-07-13 22:41:47 +0700

    minor bug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,13 +32,19 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+        <div class="flex items-center justify-between mt-4">
+            {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif --}}
+            <p class="text-sm text-left">Don't have an account yet?
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('register') }}">
+                    Register
+                </a>
+            </p>
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
--- resources/views/livewire/edit-project.blade.php
+++ resources/views/livewire/edit-project.blade.php
@@ -6,7 +6,9 @@
                     href="{{ route('user-projects', $project->user->username) }}">{{ $project->user->username }}</a>
                 | {{ $project->name }}
             </h2>
-            <x-button icon="pencil" primary wire:click="editProject" />
+            @if (auth()->user()->id == $project->user->id)
+                <x-button icon="pencil" primary wire:click="editProject" />
+            @endif
         </div>
         <x-badge lg label="{{ $project->isPrivate ? 'Private' : 'Public' }}" />
     </div>
