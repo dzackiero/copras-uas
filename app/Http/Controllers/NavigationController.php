db2b5dbb372435c98689e96a67cffdb386f968c1
dzackiero <dzakynashshar@gmail.com>
2023-07-20 21:09:47 +0700

    adding register

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -21,6 +21,34 @@ class NavigationController extends Controller
         $project = Project::firstOrCreate([
             'user_id' => $user->id,
         ]);
+
+        if(!$project->criterias){
+            Criteria::create([
+                'project_id' => $project->id,
+                'name' => 'Durability',
+                'isBenefit' => True,
+                'weight' => 5
+            ]);
+            Criteria::create([
+                'project_id' => $project->id,
+                'name' => 'Damage',
+                'isBenefit' => True,
+                'weight' => 5
+            ]);
+            Criteria::create([
+                'project_id' => $project->id,
+                'name' => 'Crowd Control',
+                'isBenefit' => True,
+                'weight' => 5
+            ]);
+            Criteria::create([
+                'project_id' => $project->id,
+                'name' => 'Difficulty',
+                'isBenefit' => True,
+                'weight' => 5
+            ]);
+        }
+
         return view('user-project', [
             'user' => $user
         ]);
