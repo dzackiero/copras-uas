60d6ce5b5d92e31a16a8cc6af7f7d74382d474b2
dzackiero <dzakynashshar@gmail.com>
2023-07-26 09:44:20 +0700

    removing delete button

--- resources/views/livewire/project-detail.blade.php
+++ resources/views/livewire/project-detail.blade.php
@@ -108,9 +108,7 @@
                         </div>
                         @if ($editCriteriaModal)
                             <x-slot name="footer">
-                                <div class="flex justify-between gap-x-4">
-                                    <x-button flat negative label="Delete" wire:click="deleteCriteria" />
-
+                                <div class="flex justify-end gap-x-4">
                                     <div class="flex">
                                         <x-button flat label="Cancel" x-on:click="close" />
                                         <x-button primary label="Save" wire:click="updateCriteria" />
